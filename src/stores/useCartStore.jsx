19dea92b3e8ec993a70ab7d0c9582e8181c14013
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-24 22:59:30 -0500

    avance

--- src/stores/useCartStore.jsx
+++ src/stores/useCartStore.jsx
@@ -1,13 +1,19 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+import { saveStorage, getStorage } from "../utils/localStorageUtils";
+
+const KEY_CART = "cart";
 
 //necesitamos create y dentro un callback con set para manejar el store
 //devtools es opcional si queremos ver que esta pasando con la extensión de redux
 const useCartStore = create(devtools((set) => ({
-  cart: [],
+  cart: getStorage(KEY_CART) || [], //si encuentra algo en el LS sera su estado inicial si no será un []
   addProductToCart: (product) => {
     set((state) => {
       const temporalCart = [...state.cart, product]; //copiamos el carrito
+
+      saveStorage(KEY_CART, temporalCart);
+
       //retornamos el cambio que deseamos se aplique
       return {
         cart: temporalCart
