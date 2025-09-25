7bc4f0cb6933c17bf7d8da805903816a67abe632
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-24 21:37:42 -0500

    avance

--- src/modules/Cart/CartView.jsx
+++ src/modules/Cart/CartView.jsx
@@ -8,7 +8,16 @@ const CartView = () => {
       <h2 className="text-4xl font-semibold mb-5">
         Prepara tu orden
       </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        {/* resumen carrito */}
+        <div>
 
+        </div>
+        {/* form */}
+        <div>
+
+        </div>
+      </div>
     </div>
   )
 }
