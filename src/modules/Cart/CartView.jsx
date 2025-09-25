698642d918b6c52538057dac62a296a36fcbd5b7
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-24 21:37:22 -0500

    avance

--- src/modules/Cart/CartView.jsx
+++ src/modules/Cart/CartView.jsx
@@ -4,7 +4,12 @@ const CartView = () => {
   const { cart } = useCartStore();
 
   return (
-    <div>CartView</div>
+    <div className="max-w-7xl mx-auto px-3 md:px-6 py-6">
+      <h2 className="text-4xl font-semibold mb-5">
+        Prepara tu orden
+      </h2>
+
+    </div>
   )
 }
 
