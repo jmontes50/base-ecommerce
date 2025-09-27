c66c76ef3c75e01d54a3e657e74d7f5b67e79287
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-26 20:45:18 -0500

    avance

--- src/stores/useCartStore.jsx
+++ src/stores/useCartStore.jsx
@@ -6,28 +6,45 @@ const KEY_CART = "cart";
 
 //necesitamos create y dentro un callback con set para manejar el store
 //devtools es opcional si queremos ver que esta pasando con la extensión de redux
-const useCartStore = create(devtools((set) => ({
-  cart: getStorage(KEY_CART) || [], //si encuentra algo en el LS sera su estado inicial si no será un []
-  addProductToCart: (product) => {
-    set((state) => {
-      //Vamos a preguntar si existe el producto en cart
-      // nos dará -1 si no encuentra nada de 0 a + con la posición del item
-      const indexIfExists = state.cart.findIndex((item) => item.id === product.id);
+const useCartStore = create(
+  devtools((set) => ({
+    cart: getStorage(KEY_CART) || [], //si encuentra algo en el LS sera su estado inicial si no será un []
+    addProductToCart: (product) => {
+      set(
+        (state) => {
+          //Vamos a preguntar si existe el producto en cart
+          // nos dará -1 si no encuentra nada de 0 a + con la posición del item
+          const indexIfExists = state.cart.findIndex(
+            (item) => item.id === product.id
+          );
 
-      console.log(indexIfExists);
-      //Si no existe, agregamos el producto como nuevo con una propiedad llamada cantidad que sea igual a 1.
-      //Si ya existe, al producto existe le incrementaremos la propiedad cantidad en +1
-      const temporalCart = [...state.cart, product]; //copiamos el carrito
+          console.log(indexIfExists);
 
-      saveStorage(KEY_CART, temporalCart);
+          //Si no existe, agregamos el producto como nuevo con una propiedad llamada cantidad que sea igual a 1.
+          if (indexIfExists === -1) {
+            const newProduct = { ...product, cantidad: 1 };
+            const temporalCart = [...state.cart, newProduct]; //copiamos el carrito
+            saveStorage(KEY_CART, temporalCart);
 
-      //retornamos el cambio que deseamos se aplique
-      return {
-        cart: temporalCart
-      }
-      //agregamos los valores para devtools
-    }, false, "cart/addProductToCart")
-  }
-})))
+            //retornamos el cambio que deseamos se aplique
+            return {
+              cart: temporalCart,
+            };
+          } else {
+            //Si ya existe nos dará 0,1,2,3,... , el producto existe, le incrementaremos la propiedad cantidad en +1
+            const temporalCart = {...state.cart};
+            temporalCart[indexIfExists].cantidad++;
+
+            return temporalCart,
+          }
+
+          //agregamos los valores para devtools
+        },
+        false,
+        "cart/addProductToCart"
+      );
+    },
+  }))
+);
 
 export default useCartStore;
