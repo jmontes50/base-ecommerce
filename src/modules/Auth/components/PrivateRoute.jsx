abc49e880c84ef16c5f4af5cf355388e65e25aa5
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-29 20:47:26 -0500

    avance

---
+++ src/modules/Auth/components/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import useAuthStore from "../../../stores/useAuthStore";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = () => {
+  return (
+    <div>PrivateRoute</div>
+  )
+}
+
+export default PrivateRoute
