25e3b767ac5d2dfc76b0f8558913b2c1edae4c2a
Jorge Montesinos <osmarmontes50@gmail.com>
2025-09-29 19:40:24 -0500

    avance

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './modules/ui/Navbar'
 import HomeView from './modules/Home/HomeView'
 import CartView from './modules/Cart/CartView'
 import RegisterView from './modules/Auth/RegisterView'
+import NotFoundView from './modules/NotFound/NotFoundView'
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
         <Route path="/" element={<HomeView />} />
         <Route path="/cart" element={<CartView />} />
         <Route path="/register" element={<RegisterView />} />
+        {/* 404, esta ruta tiene que estar al final */}
+        <Route path="*" element={<NotFoundView />} />
       </Routes>
 
       <ToastContainer />
---
+++ src/modules/NotFound/NotFoundView.jsx
@@ -0,0 +1,9 @@
+const NotFoundView = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h2 className="text-4xl font-bold">404 No se encontró lo que buscas</h2>
+    </div>
+  )
+}
+
+export default NotFoundView
